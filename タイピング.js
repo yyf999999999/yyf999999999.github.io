6702544411e21509472248c552ea4b5fa8afc3d7
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2023-09-24 23:44:33 +0900

    Update タイピング.js
    入力した文字に応じて字幕を変更するようにした

--- タイピング.js
+++ タイピング.js
@@ -1,45 +1,44 @@
-const jnSentence1=document.getElementById("jnSentence1");//元の日本語文
-const enSentence=document.getElementById("enSentence");//ローマ字に変換した文
-const jnSentence2=document.getElementById("jnSentence2");//入力した日本語文
-const jnContent=document.getElementById("jnContent");//入力フォーム
+const jnSentence1=document.getElementById("jnSentence1");
+const enSentence=document.getElementById("enSentence");
+const jnSentence2=document.getElementById("jnSentence2");
+const jnContent=document.getElementById("jnContent");
 var jn=["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と",
-"な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ",
-"わ","ゐ","ゑ","を","ん","ヴ","が","ぎ","ぐ","げ","ご","ざ","じ","ず","ぜ","ぞ","だ","ぢ","づ","で","ど",
-"ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ","いぇ","うぁ","うぃ","うぇ","うぉ","きゃ","きぃ","きゅ","きぇ","きょ",
-"くぁ","くぃ","くぅ","くぇ","くぉ","くゃ","くゅ","くょ","しゃ","しぃ","しゅ","しぇ","しょ","すぁ","すぃ","すぅ","すぇ","すぉ",
-"ちゃ","ちぃ","ちゅ","ちぇ","ちょ","つぁ","つぃ","つぇ","つぉ","てゃ","てぃ","てゅ","てぇ","てょ",
-"とぁ","とぃ","とぅ","とぇ","とぉ","にゃ","にぃ","にゅ","にぇ","にょ","ひゃ","ひぃ","ひゅ","ひぇ","ひょ",
-"ふぁ","ふぃ","ふぇ","ふぉ","みゃ","みぃ","みゅ","みぇ","みょ","りゃ","りぃ","りゅ","りぇ","りょ","ヴぁ","ヴぃ","ヴぇ","ヴぉ",
-"ぎゃ","ぎぃ","ぎゅ","ぎぇ","ぎょ","ぐぁ","ぐぃ","ぐぅ","ぐぇ","ぐぉ","じゃ","じぃ","じゅ","じぇ","じょ",
-"ぢゃ","ぢぃ","ぢゅ","ぢぇ","ぢょ","でゃ","でぃ","でゅ","でぇ","でょ","どぁ","どぃ","どぅ","どぇ","どぉ",
-"びゃ","びぃ","びゅ","びぇ","びょ","ぴゃ","ぴぃ","ぴゅ","ぴぇ","ぴょ",
-"ぁ","ぃ","ぅ","ぇ","ぉ","ゃ","ゅ","ょ","っ","ゎ","ヵ","ヶ","ー","、","。","・"],//文字一覧(日本語)
+        "な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ",
+        "わ","ゐ","ゑ","を","ん","ヴ","が","ぎ","ぐ","げ","ご","ざ","じ","ず","ぜ","ぞ","だ","ぢ","づ","で","ど",
+        "ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ","いぇ","うぁ","うぃ","うぇ","うぉ","きゃ","きぃ","きゅ","きぇ","きょ",
+        "くぁ","くぃ","くぅ","くぇ","くぉ","くゃ","くゅ","くょ","しゃ","しぃ","しゅ","しぇ","しょ","すぁ","すぃ","すぅ","すぇ","すぉ",
+        "ちゃ","ちぃ","ちゅ","ちぇ","ちょ","つぁ","つぃ","つぇ","つぉ","てゃ","てぃ","てゅ","てぇ","てょ",
+        "とぁ","とぃ","とぅ","とぇ","とぉ","にゃ","にぃ","にゅ","にぇ","にょ","ひゃ","ひぃ","ひゅ","ひぇ","ひょ",
+        "ふぁ","ふぃ","ふぇ","ふぉ","みゃ","みぃ","みゅ","みぇ","みょ","りゃ","りぃ","りゅ","りぇ","りょ","ヴぁ","ヴぃ","ヴぇ","ヴぉ",
+        "ぎゃ","ぎぃ","ぎゅ","ぎぇ","ぎょ","ぐぁ","ぐぃ","ぐぅ","ぐぇ","ぐぉ","じゃ","じぃ","じゅ","じぇ","じょ",
+        "ぢゃ","ぢぃ","ぢゅ","ぢぇ","ぢょ","でゃ","でぃ","でゅ","でぇ","でょ","どぁ","どぃ","どぅ","どぇ","どぉ",
+    "びゃ","びぃ","びゅ","びぇ","びょ","ぴゃ","ぴぃ","ぴゅ","ぴぇ","ぴょ",
+    "ぁ","ぃ","ぅ","ぇ","ぉ","ゃ","ゅ","ょ","っ","ゎ","ヵ","ヶ","ー","、","。","・"],
     en=[["a"],["i","yi"],["u","wu","whu"],["e"],["o"],["ka","ca"],["ki"],["ku","qu","cu"],["ke"],["ko","co"],
-    ["sa"],["si","shi","ci"],["su"],["se","ce"],["so"],["ta"],["ti","chi"],["tu","tsu"],["te"],["to"],
-    ["na"],["ni"],["nu"],["ne"],["no"],["ha"],["hi"],["hu","fu"],["he"],["ho"],["ma"],["mi"],["mu"],["me"],["mo"],
-    ["ya"],["yu"],["yo"],["ra"],["ri"],["ru"],["re"],["ro"],["wa"],["wyi"],["wye"],["wo"],["nn","n","xn"],
-    ["vu"],["ga"],["gi"],["gu"],["ge"],["go"],["za"],["zi","ji"],["zu"],["ze"],["zo"],
-    ["da"],["di"],["du"],["de"],["do"],["ba"],["bi"],["bu"],["be"],["bo"],["pa"],["pi"],["pu"],["pe"],["po"],
-    ["ye"],["wha"],["wi","whi"],["we","whe"],["who"],["kya"],["kyi"],["kyu"],["kye"],["kyo"],
-    ["qa","qwa","kwa"],["qi","qwi"],["qwu"],["qe","qwe"],["qo","qwo"],["qya"],["qyu"],["qyo"],
-    ["sya","sha"],["syi"],["syu","shu"],["sye","she"],["syo","sho"],["swa"],["swi"],["swi"],["swe"],["swo"],
-    ["tya","cha","cya"],["tyi","cyi"],["tyu","chu","cyu"],["tye","che","cye"],["tyo","cho","cyo"],
-    ["tsa"],["tsi"],["tse"],["tso"],["tha"],["thi"],["thu"],["the"],["tho"],
-    ["twa"],["twi"],["twu"],["twe"],["two"],["nya"],["nyi"],["nyu"],["nye"],["nyo"],
-    ["hya"],["hyi"],["hyu"],["hye"],["hyo"],["fa"],["fi"],["fe"],["fo"],
-    ["mya"],["myi"],["myu"],["mye"],["myo"],["rya"],["ryi"],["ryu"],["rye"],["ryo"],
-    ["va"],["vi"],["ve"],["vo"],["gya"],["gyi"],["gyu"],["gye"],["gyo"],["gwa"],["gwi"],["gwu"],["gwe"],["gwo"],
-    ["zya","ja","jya"],["zyi","jwi"],["zyu","ju","jyu"],["zye","je","jye"],["zyo","jo","jyo"],
-    ["dya"],["dyi"],["dyu"],["dye"],["dyo"],["dha"],["dhi"],["dhu"],["dhe"],["dho"],
-    ["dwa"],["dwi"],["dwu"],["dwe"],["dwo"],["bya"],["byi"],["byu"],["bye"],["byo"],
-    ["pya"],["pyi"],["pyu"],["pye"],["pyo"],
-    ["xa","la"],["xi","li","xyi","lyi"],["xu","lu"],["xe","le","xye","lye"],["xo","lo"],
-    ["xya","lya"],["xyu","lyu"],["xyo","lyo"],["xtu","xtsu","ltu","ltsu"],["xwa","lwa"],
-    ["xka","lka"],["xke","lke"],["-"],[","],["."],["/"]],//日本語に対応したローマ字
+        ["sa"],["si","shi","ci"],["su"],["se","ce"],["so"],["ta"],["ti","chi"],["tu","tsu"],["te"],["to"],
+        ["na"],["ni"],["nu"],["ne"],["no"],["ha"],["hi"],["hu","fu"],["he"],["ho"],["ma"],["mi"],["mu"],["me"],["mo"],
+        ["ya"],["yu"],["yo"],["ra"],["ri"],["ru"],["re"],["ro"],["wa"],["wyi"],["wye"],["wo"],["nn","n","xn"],
+        ["vu"],["ga"],["gi"],["gu"],["ge"],["go"],["za"],["zi","ji"],["zu"],["ze"],["zo"],
+        ["da"],["di"],["du"],["de"],["do"],["ba"],["bi"],["bu"],["be"],["bo"],["pa"],["pi"],["pu"],["pe"],["po"],
+        ["ye"],["wha"],["wi","whi"],["we","whe"],["who"],["kya"],["kyi"],["kyu"],["kye"],["kyo"],
+        ["qa","qwa","kwa"],["qi","qwi"],["qwu"],["qe","qwe"],["qo","qwo"],["qya"],["qyu"],["qyo"],
+        ["sya","sha"],["syi"],["syu","shu"],["sye","she"],["syo","sho"],["swa"],["swi"],["swi"],["swe"],["swo"],
+        ["tya","cha","cya"],["tyi","cyi"],["tyu","chu","cyu"],["tye","che","cye"],["tyo","cho","cyo"],
+        ["tsa"],["tsi"],["tse"],["tso"],["tha"],["thi"],["thu"],["the"],["tho"],
+        ["twa"],["twi"],["twu"],["twe"],["two"],["nya"],["nyi"],["nyu"],["nye"],["nyo"],
+        ["hya"],["hyi"],["hyu"],["hye"],["hyo"],["fa"],["fi"],["fe"],["fo"],
+        ["mya"],["myi"],["myu"],["mye"],["myo"],["rya"],["ryi"],["ryu"],["rye"],["ryo"],
+        ["va"],["vi"],["ve"],["vo"],["gya"],["gyi"],["gyu"],["gye"],["gyo"],["gwa"],["gwi"],["gwu"],["gwe"],["gwo"],
+        ["zya","ja","jya"],["zyi","jwi"],["zyu","ju","jyu"],["zye","je","jye"],["zyo","jo","jyo"],
+        ["dya"],["dyi"],["dyu"],["dye"],["dyo"],["dha"],["dhi"],["dhu"],["dhe"],["dho"],
+        ["dwa"],["dwi"],["dwu"],["dwe"],["dwo"],["bya"],["byi"],["byu"],["bye"],["byo"],
+        ["pya"],["pyi"],["pyu"],["pye"],["pyo"],
+        ["xa","la"],["xi","li","xyi","lyi"],["xu","lu"],["xe","le","xye","lye"],["xo","lo"],
+        ["xya","lya"],["xyu","lyu"],["xyo","lyo"],["xtu","xtsu","ltu","ltsu"],["xwa","lwa"],
+        ["xka","lka"],["xke","lke"],["-"],[","],["."],["/"]],
     enCharacter=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
-    "q","r","s","t","u","v","w","x","y","z","-",",",".","/"],//文字一覧(ローマ字)
+                "q","r","s","t","u","v","w","x","y","z","-",",",".","/"],
     jnInput,jnVowel=jn.slice(0,5),enVowel=en.slice(0,5),enInput="";
-//jnINput=>タイピングされた日本語を表示,jnVowel=>日本語の母音,enVowel=>英語の母音,enInput=>タイピングされた純粋な文を表示
 for (i=0;i<enVowel.length;i++){
     enVowel[i]=enVowel[i][0];
 }
@@ -98,16 +97,20 @@ function enToJn(letter){
 function input(e){
     if (e.key!="Enter"){
         enInput+=e.key;
-        var inp=enToJn(enInput).split(""),ans=enToJn(enAnswer).split(""),n=false;
+        var inp=enToJn(enInput).split(""),ans=enToJn(enAnswer).split(""),printCharacter=enInput,prChD=false;
         while (inp[0]==ans[0]/*&&jn.includes(inp[0])*/&&ans.length!=0){
             inp=inp.slice(1);ans=ans.slice(1);
         }
-        if (enToJn(enInput).split("").slice(0,-1).join("")!=enToJn(enAnswer).split("").join("")){
+        for (i=0;i<inp.length;i++){
+            printCharacter=printCharacter.slice(0,-1);
+        }
+        if (enToJn(enInput).slice(0,-1)!==enToJn(enAnswer)){
             if (jnVowel.includes(inp.join(""))){
                 inp[0]=enVowel[jnVowel.indexOf(inp[0])];
             }
             if (jn.includes(ans[0]+ans[1])){
                 var characters=[en[jn.indexOf(ans[0]+ans[1])],en[jn.indexOf(ans[2])]];
+                prChD=true;
             }else if (jn.includes(ans[0])){
                 var characters=[en[jn.indexOf(ans[0])],en[jn.indexOf(ans[1])]];
             }else{
@@ -123,22 +126,33 @@ function input(e){
                             for (n=0;n<characters[1].length;n++){
                                 character2=characters[1][n].split("");
                                 if (inp[0]==character2[0]){
-                                    character=character2[0]+character2;
+                                    character=(character2[0]+character2.join("")).split("");
                                     break;
                                 }
                             }
                         }
                         tfI=tfI&&character[j]==inp[j];
                     }
+                    if (tfI&&!tfC){
+                        if (ans[0]=="っ"){
+                            ans[0]=jnToEn(ans[1])[0];
+                            ans.unshift("");
+                        }else{
+                            printCharacter+=character.join("");
+                        }
+                        if (prChD) ans.splice(0,1);
+                    }
                     tfC=tfC||tfI;
                 }
                 if (!tfC){
-                    enInput=enInput.split("").slice(0,-1).join("");
+                    enInput=enInput.slice(0,-1);
+                    ans.unshift("");
                 }
             }
         }else{
-            enInput=enInput.split("").slice(0,-1).join("");
+            enInput=enInput.slice(0,-1);
         }
+        enSentence.innerHTML="ローマ字に変換した文 : "+jnToEn(printCharacter+ans.splice(1).join(""));
         jnSentence2.innerHTML="入力した日本語分 : "+enToJn(enInput);
     }
 }
